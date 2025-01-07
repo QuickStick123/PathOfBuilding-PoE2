d44d4c962e179e2338563f73cbf7e856790e7210
Thomas <31533893+QuickStick123@users.noreply.github.com>
2025-01-07 16:52:23 +1100

    Remove special corruptions from ModUniques.lua

--- src/Export/Scripts/mods.lua
+++ src/Export/Scripts/mods.lua
@@ -205,7 +205,7 @@ end)
 writeMods("../Data/ModUnique.lua", function(mod)
 	return (mod.Domain == 1 or mod.Domain == 2 or mod.Domain == 11 or mod.Domain == 22) and mod.GenerationType == 3
 	and (mod.Family[1] and mod.Family[1].Id ~= "AuraBonus" or not mod.Family[1])
-	and not mod.Id:match("^Synthesis") and not mod.Id:match("Royale") and not mod.Id:match("Cowards") and not mod.Id:match("Map") and not mod.Id:match("Ultimatum")
+	and not mod.Id:match("^Synthesis") and not mod.Id:match("Royale") and not mod.Id:match("Cowards") and not mod.Id:match("Map") and not mod.Id:match("Ultimatum") and not mod.Id:match("SpecialCorruption")
 end)
 
 
